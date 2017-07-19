eedae2c6d718f10252f098cff4d9ec803dafd469
Cisco-Nokia <nokia_7373@foxmail.com>
2017-07-19 16:46:01 +0800

    add nat address

--- base_ftp_server.py
+++ base_ftp_server.py
@@ -19,6 +19,12 @@ authorizer.add_anonymous("/home/lynn")
 #初始化ftp句柄
 handler = FTPHandler
 handler.authorizer = authorizer
+
+# 添加返回给用户的公网ip，由于ftp服务端会在被动模式中，将自己本机的IP地址返回给客户端，
+# 但有时候，服务器是通过防火墙进行了地址映射的，因此会将自己的私有IP返回给客户端，从而导致客户端无法连接，
+# 这个问题，一般在ftp客户端软件上可以自动或手动修正，但我们最好是告诉客户端公网IP为好。
+# handler.masquerade_address = "1.1.1.1"
+
 # 添加FTP被动模式的端口
 handler.passive_ports = range(2000, 2333)
 # 监听ip和端口
